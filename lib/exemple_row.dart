55b8eff2ceecd7e9542b71a117f7e3e7c2c711f4
caioomega <caio.mega@aluno.senai.br>
2026-04-07 07:34:22 -0300

    first commit

---
+++ lib/exemple_row.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar:AppBar(
+          title: Text('Exemplo row'),
+        ),
+
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               Icon(Icons.home, size: 20),
+
+               SizedBox(width: 20),
+
+               Icon(Icons.favorite, size:40),
+
+                SizedBox(width: 20),
+
+                Icon(Icons.settings, size: 40)
+
+            ]
+              ),
+          ),
+        ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/logo.png"),
+              fit: BoxFit.contain
+            )
+          ),
+        ),
+      )
+    );
+  }
+}
